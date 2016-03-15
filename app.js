35d197de2627d7eb954e275467f7e2c965f7fac1
dimka2014 <dimabelyaev27@gmail.com>
2016-03-15 16:35:08 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express')
+  , app = express()
+  , bodyParser = require('body-parser')
+  , port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(require('./controllers'));
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+});
